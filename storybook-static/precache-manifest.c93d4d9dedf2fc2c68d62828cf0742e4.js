3513d339d4f934238f7708c7b04b1588fa706b0b
mohamed sendi <mohamed.sendi@ingenico.com>
2020-10-16 10:16:54 +0200

    taskbox UI

---
+++ storybook-static/precache-manifest.c93d4d9dedf2fc2c68d62828cf0742e4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43367bbec04b9b3539769872e1562b37",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.796ebba03dec18efc208.bundle.js"
+  },
+  {
+    "url": "runtime~main.796ebba03dec18efc208.bundle.js"
+  },
+  {
+    "revision": "36a0605062e0d10ccea2",
+    "url": "static/css/main.d33b1d5f.chunk.css"
+  },
+  {
+    "revision": "d43eb968e25e81ce0af629bad1069a19",
+    "url": "static/media/code-brackets.d43eb968.svg"
+  },
+  {
+    "revision": "8104e6f5a95cbe9c9615b47daf881fc7",
+    "url": "static/media/colors.8104e6f5.svg"
+  },
+  {
+    "revision": "3f9a1398f6372bd43806b239c70f4759",
+    "url": "static/media/comments.3f9a1398.svg"
+  },
+  {
+    "revision": "d3dc567bd3e26a031bfd7fab1e765508",
+    "url": "static/media/direction.d3dc567b.svg"
+  },
+  {
+    "revision": "e3812c4ff33e4e7ae9686a7fcfef393f",
+    "url": "static/media/flow.e3812c4f.svg"
+  },
+  {
+    "revision": "5ba97e9be21511d352f0056b1970ce21",
+    "url": "static/media/plugin.5ba97e9b.svg"
+  },
+  {
+    "revision": "3452b916238c37efec707e6979db3556",
+    "url": "static/media/repo.3452b916.svg"
+  },
+  {
+    "revision": "66f4b748553f89f290a3247a913072af",
+    "url": "static/media/stackalt.66f4b748.svg"
+  },
+  {
+    "url": "vendors~main.796ebba03dec18efc208.bundle.js"
+  },
+  {
+    "url": "vendors~main.796ebba03dec18efc208.bundle.js.LICENSE.txt"
+  }
+]);
